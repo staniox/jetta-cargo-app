c9470a3ac10999248e4a51a1295ee2270ae881f3
gomes <brunogomes1958@gmail.com>
2021-03-16 16:55:28 -0300

    terminado app

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -3,11 +3,11 @@
       <b-form @submit="onSubmit" >
 
 
-        <b-container class="bv-example-row">
+        <b-container class="d-flex flex-row flex-wrap ">
 
-
-          <b-row v-bind:key="index" v-for="(value,name,index) in form">
-            <b-container><h5 class="display-12 bg-dark text-light opacity-on-hover">{{ value.label }}</h5></b-container>
+          <div class="form-container">
+          <b-row class="" v-bind:key="index" v-for="(value,name,index) in form">
+            <b-container><h5 class="display-12 np opacity-on-hover">{{ value.label }}</h5></b-container>
             <b-col>
               <b-form-group
                   :id="'input-amount-group-'+name"
@@ -15,6 +15,7 @@
                   :label-for="'input-amount-'+name"
               >
                 <b-form-input
+                    :disabled = "value.label === 'Container'"
                     :id="'input-amount-'+name"
                     v-model="form[name].amount"
                     type="number"
@@ -72,10 +73,19 @@
           <hr class="my-4">
 
           <b-button class="bg-dark text-light opacity-on-hover" type="submit" > Submit </b-button>
-          <div>
+          </div>
+          <div class="result">
             <b-overlay :show="isLoading" rounded="sm">
           <b-card class="mt-3" header="Result" v-show="show">
-            <pre class="m-0">{{ response}}</pre>
+            <pre class="m-0">
+              <span> Volume do Container = </span>{{ response.volume}} %
+              <span> Qtd Box1 = </span>{{ response.count.box1}}
+              <span> Qtd Box2 = </span>{{ response.count.box2}}
+              <span> Qtd Box3 = </span>{{ response.count.box3}}
+              <span> Posições Box1 = </span>{{ JSON.stringify(response.box1)}}
+              <span> Posições Box2 = </span>{{ JSON.stringify(response.box2)}}
+              <span> Posições Box3 = </span>{{ JSON.stringify(response.box3)}}
+            </pre>
           </b-card>
             </b-overlay>
           </div>
@@ -99,7 +109,7 @@ export default {
         box3: {label:'Box 3',amount:'15',length:'0.20',width:'0.6',height:'0.41'},
         container: {label:'Container',amount:'1',length:'2',width:'4',height:'1.5'},
       },
-      response:{},
+      response:{count:{}},
       show:false,
       isLoading: false
     }
@@ -131,5 +141,19 @@ h5{
 .opacity-on-hover:hover{
   opacity: 0.8;
   transition: opacity 30ms;
+}
+.result{
+  width: 100% !important;
+}
+.form-container{
+  width: 100% !important;
+}
+@media (min-width: 1000px) {
+  .form-container{
+    width: 50%;
+  }
+  .result{
+    width: 50%;
+  }
 }
 </style>
--- src/services/axios.js
+++ src/services/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 Vue.use(VueAxios, axios)
-
+const hostname = window.location.hostname
 export const app = axios.create({
-    baseURL:'http://localhost:8081/api/'
+    baseURL: 'http://' + hostname+':8081/api/'
 })
